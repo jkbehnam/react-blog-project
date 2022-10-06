cc426639bb40231805e42aa0c7ba9b3849f91687
behnam b <jkbehnam@gmail.com>
2022-10-06 16:42:11 +0330

    minor changes

---
+++ src/components/Feed.jsx
@@ -0,0 +1,20 @@
+import { Container } from "@mui/system";
+import { useTheme } from "@mui/material/styles";
+import Post from "./Post";
+import Add from './Add'
+
+function Feed() {
+  const theme=useTheme()
+  return (
+    <Container sx={{pt:theme.spacing(9) }}>
+      <Post/>
+      <Post/>
+      <Post/>
+      <Post/>
+      <Post/>
+      <Add/>
+    </Container>
+  );
+}
+
+export default Feed;
